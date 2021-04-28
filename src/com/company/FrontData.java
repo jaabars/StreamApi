93f5618fe475452996bc83f0fdd71a12ce6811bc
jaabars <aziret3405@gmail.com>
2021-04-28 18:29:56 +0600

    Initial commit

---
+++ src/com/company/FrontData.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class FrontData {
+    private int userId;
+    private int id;
+    private String title;
+
+    public FrontData(int userId, int id, String title) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "FrontData{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,73 @@
+package com.company;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+
+    /**
+     * skip
+     * limit
+     * sorted
+     */
+
+    public static void main(String[] args) {
+        List<String> carList = new ArrayList<>();
+       /* carList.add("BMW");
+        carList.add("RR");
+        carList.add("Maserati");*/
+       /* carList.add("Ferrari");
+        carList.add("Lada");
+        carList.add("Volga");
+        carList.add("Dodge");
+        carList.add("BMW");
+        carList.add("BMW");
+        carList.add("BMW");
+        for (int i =0;i<carList.size(); i++){
+            System.out.println(carList.get(i));
+        }
+        System.out.println("______________________________________");
+        System.out.println("**************************************");
+        carList.stream().forEach(System.out::println);
+
+        for (int i=0;i<carList.size();i++){
+            if (carList.get(i).equals("BMW")) {
+                System.out.println(carList.get(i));
+            }
+        }
+        carList.stream().filter(car->car.equals("BMW")).forEach(System.out::println);
+*/
+/*
+        try {
+            String url = "https://jsonplaceholder.typicode.com/posts";
+            HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(url).openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setRequestProperty("User-Agent","Mozilla/5.0");
+            int responseCode = httpURLConnection.getResponseCode();
+            System.out.println(responseCode);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while((line=bufferedReader.readLine())!=null){
+                response.append(line);
+            }
+            System.out.println(response.toString());
+            Gson gson = new Gson();
+            FrontData[] frontData = new Gson().fromJson(String.valueOf(response),FrontData[].class);
+            for (FrontData f:frontData){
+                System.out.println(f);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+}
